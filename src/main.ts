8d43b06e180d0201cf7faeb1c5484ed2b729a41b
Kamal <elesgerlikamal04@gmail.com>
2022-12-23 21:13:50 +0400

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   await app.listen(3000, () => console.log('Server running on port 3000'));
 }
 bootstrap();
